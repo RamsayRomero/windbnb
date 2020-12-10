83db481dc56882a32513ceb068e02b8246f913a7
Ramsay Romero <Ramsay.Romero@gmail.com>
2020-12-09 17:01:04 -0800

    changed config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <div className='bg-black text-white'>App</div>;
+  return <div className='bg-black text-blue-500'>App</div>;
 };
 
 export default App;
